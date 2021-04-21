94d0d1f837de47111e747a9d308177c25909c10c
shab1r <0985130@hr.nl>
2021-04-21 14:02:09 +0200

    code uit main naar virtual gekopieerd

--- Mini Battery interface.py
+++ Mini Battery interface.py
@@ -1,40 +1,10 @@
-#yooo
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-# from adafruit_motorkit import MotorKit
-# from adafruit_motor import motor as MotorControl
-import time
-import threading
-from threading import Thread
-from time import sleep
-import random
-import math
-import turtle
-import csv
-import matplotlib
-matplotlib.use("tkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
-from matplotlib.backend_bases import key_press_handler
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import pylab
-import numpy as np
-import os
-from tkinter.filedialog import askdirectory
-# from ExpanderPi import ADC
-# from ExpanderPi import DAC
-import time
-from timeit import default_timer as timer
-import glob,os
+from lib import *
 
 # kit = MotorKit()
 mp = []
 # adc = ADC()
 # dac = DAC(2)
-plt.ion()
+# plt.ion()
 min_y = 0
 max_y = 40
 
---
+++ lib.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+# from adafruit_motorkit import MotorKit
+# from adafruit_motor import motor as MotorControl
+import time
+import threading
+from threading import Thread
+from time import sleep
+import random
+import math
+import turtle
+import csv
+import matplotlib
+matplotlib.use("tkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import pylab
+import numpy as np
+import os
+from tkinter.filedialog import askdirectory
+# from ExpanderPi import ADC
+# from ExpanderPi import DAC
+import time
+from timeit import default_timer as timer
+import glob,os
